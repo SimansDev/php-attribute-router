52623cabd9937dda5fe5551b7466cbd2607c658f
Alan Simons <simanovv.dev@gmail.com>
2024-07-14 18:12:23 +0200

    Cleaning

--- src/Attribute/src/Dispatcher.php
+++ src/Attribute/src/Dispatcher.php
@@ -21,8 +21,7 @@ class Dispatcher
         private string $controllersNamespaceRoot,
         private string $controllersPath,
         private string $cachePath = '',
-    )
-    {
+    ) {
         $this->init();
     }
 
@@ -32,15 +31,13 @@ class Dispatcher
         $key = $this->getRoutesKey($request->getMethod(), $route->getPath());
         if (!key_exists($key, $routes)) {
             if (str_contains($key, ':')) {
-                list($method, $route) = explode(':',$key);
+                list($method, $route) = explode(':', $key);
                 $key = sprintf('[%s] %s', $method, $route);
             }
-
             throw new RouteNotFoundException("Route {$key} Not Found");
         }
-
         list($class, $method) = explode('::', $routes[$key]);
-        return new RouteDto(class:$class,method:$method, type:'');
+        return new RouteDto(class: $class, method: $method, type: '');
     }
 
 
@@ -53,9 +50,10 @@ class Dispatcher
     private function init()
     {
         if (!$this->isControllerExist()) {
-            throw new NotConfiguredServiceException('Controller path is not specified or default namespace is not specified. Abort dispatching');
+            throw new NotConfiguredServiceException(
+                'Controller path is not specified or default namespace is not specified. Abort dispatching'
+            );
         }
-
         $this->collectRoutes();
         if (strlen($this->cachePath) === 0 && !is_dir($this->cachePath)) {
             $this->cacheRoutesCollection();
@@ -77,36 +75,30 @@ class Dispatcher
         if (isset(self::$routes) and count(self::$routes) > 0) {
             return;
         }
-
         $files = scandir($this->controllersPath);
         if (!is_array($files)) {
             throw new DirectoryEmptyException('Was not able to scan the directory');
         }
-
         $routes = [];
         foreach ($files as $file) {
             if (in_array($file, ['.', '..'])) {
                 continue;
             }
-
-            if (!str_ends_with($file,'.php')) {
+            if (!str_ends_with($file, '.php')) {
                 continue;
             }
-
             if (is_dir(sprintf('%s/%s', $this->controllersPath, $file))) {
                 //@todo add directory recursive logic
                 continue;
             }
-
             $className = substr($file, 0, -4);
             try {
                 $reflection = new ReflectionClass($this->controllersNamespaceRoot . $className);
-                foreach($reflection->getMethods() as $method) {
-                    foreach ($method->getAttributes() as $key => $attribute) {
+                foreach ($reflection->getMethods() as $method) {
+                    foreach ($method->getAttributes() as $attribute) {
                         if (Route::class !== $attribute->getName()) {
                             continue;
                         }
-
                         $targetController = $reflection->getName();
                         $methodName = $method->getName();
                         $controllerPlace = sprintf('%s::%s', $targetController, $methodName);
@@ -115,9 +107,9 @@ class Dispatcher
                         $routes[$path] = $controllerPlace;
                     }
                 }
-            } catch (ReflectionException $exception) {}
+            } catch (ReflectionException $exception) {
+            }
         }
-
         self::$routes = $routes;
     }
 
@@ -128,16 +120,13 @@ class Dispatcher
         if ($cacheFileStream === false) {
             throw new Exception('Not able to open stream');
         }
-
+        $routes = self::$routes;
         $content = fread($cacheFileStream, filesize($this->cachePath));
         fclose($cacheFileStream);
-
         $data = json_decode($content, true);
-        $diff = array_diff($this->routes, $data);
-
+        $diff = array_diff($routes, $data);
         if (count($diff) > 0) {
-            $data = json_encode($this->routes);
-
+            $data = json_encode($routes);
             $cacheFileStream = fopen($this->cachePath, 'w+');
             fwrite($cacheFileStream, $data);
             fclose($cacheFileStream);
@@ -146,17 +135,8 @@ class Dispatcher
 
     private function getRoutesCollection(): array
     {
-        $this->isCachedRoutes() ?
-            $this->getCollectionRoutesFromCache()
-            : $this->collectRoutes();
-
-        return $this->routes;
-    }
-
-    private function getRootNamespace(): string
-    {
-        $currentNamespace = __NAMESPACE__;
-        return substr($currentNamespace, 0, -(strlen($currentNamespace) - (strrpos($currentNamespace, '\\') + 1)));
+        $this->isCachedRoutes() ? $this->getCollectionRoutesFromCache() : $this->collectRoutes();
+        return self::$routes;
     }
 
     private function isCachedRoutes()
@@ -170,7 +150,6 @@ class Dispatcher
         if ($cache === false) {
             return [];
         }
-
         $routes = json_decode($cache, true);
         return $routes;
     }
